fd5a21f713c77e0ec8dd2ad974da23f9b45db60a
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-09-14 19:00:37 -0300

    Order and Product Serializers

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order',
+    'product',
+    'rest_framework',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

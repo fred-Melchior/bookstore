3fa69ce51f363577bfc095a4621ccadcb1726a0f
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-09-18 10:26:46 -0300

    Pagination config

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 
 #(?<version>(v1|v2))
 
 urlpatterns = [
+    path('__debug__', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     re_path('bookstore/', include('order.urls')),
     re_path('bookstore/', include('product.urls')),

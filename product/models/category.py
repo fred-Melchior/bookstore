16c7af3000d2b8ac375a1e6a7e6861dd9eed1b0b
fred-Melchior <frederico.melchior@gmail.com>
2023-09-28 18:50:29 -0300

    Integration with GitHub Actions v11

--- product/models/category.py
+++ product/models/category.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=200, blank=True, null=True)
     active = models.BooleanField(default=True)
+    id = models.IntegerField(max_length=25)
 
     def __unicode__(self):
         return self.title
